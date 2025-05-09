df2976ce4a32ca4fc077441b99ab93f9fd3dc499
wayne <waynekim07@gmail.com>
2025-05-09 08:30:07 +0300

    JavaScript constant variables / document variables

---
+++ script.js
@@ -0,0 +1,16 @@
+// Input variables
+const titleInp = document.querySelector(".title-inp input");
+const dateInp = document.querySelector(".date-inp input");
+const locationInp = document.querySelector(".location-inp input");
+const attendeesInp = document.querySelector(".attendees-inp input");
+
+// Btn variables
+const eventBtn = document.querySelector("#event-btn");
+const calendarBtn = document.querySelector("#calendar-btn");
+const addEventBtn = document.querySelector("#add-event")
+const clearEventBtn = document.querySelector("#clear-event")
+
+// list container variables
+const weeksEventList  = document.querySelector(".weeksEvents-list")
+const eventListTable = document.querySelector(".eventList-section table")
+

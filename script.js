a075383a52e9e8d821bc7292d6db8b2614afc9a5
wayne <waynekim07@gmail.com>
2025-05-10 13:22:14 +0300

    Added a showWeekEvents function

--- script.js
+++ script.js
@@ -7,64 +7,116 @@ const attendeesInp = document.querySelector(".attendees-inp input");
 // Btn variables
 const eventBtn = document.querySelector("#event-btn");
 const calendarBtn = document.querySelector("#calendar-btn");
-const addEventBtn = document.querySelector("#add-Event")
-const clearEventBtn = document.querySelector("#clear-Event")
+const addEventBtn = document.querySelector("#add-Event");
+const clearEventBtn = document.querySelector("#clear-Event");
 
 // list container variables
-const eventListTable = document.querySelector(".eventList-section table tbody")
-const weekEventsList  = document.querySelector(".weekEvents-list")
-const events = []
-const weekEvents = []
+const eventListTable = document.querySelector(".eventList-section table tbody");
+const weekEventsList = document.querySelector(".weekEvents-list");
+const events = [];
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let weekEvents;
+
 // Functions
-    // renderPage : Renders the page's origin state
-    const renderPage = ()=>{
-
-        eventListTable.innerHTML = events.map(event=>event).join("")
-        weekEvents.innerHTML = weekEvents.map(event=>event).join("")
-        
-    }
-
-    
-
-    // addEvent : Adds Events from the input fields to the event list
-    const addEvent = ()=>{
-        const newTableRow  = `
+// renderPage : Renders the page's origin state
+const renderPage = () => {
+  showWeekEvents();
+  eventListTable.innerHTML = events
+    .map(
+      (event) => `
             <tr>
-                <td>${dateInp.value}</td>
-                <td>${locationInp.value}</td>
-                <td>${titleInp.value}</td>
-                <td>${attendeesInp.value}</td>
-                <td><button class="btn" onclick="deleteEvent()">Delete</button></td>    
+                <td>${event.date}</td>      
+                <td>${event.location}</td>      
+                <td>${event.title}</td>      
+                <td>${event.attendees}</td>      
+                <td>
+                    <button class="btn"onclick="deleteEvent(this)">Delete</button>
+                </td>
             </tr>
-        `;
-        events.push(newTableRow);
-        renderPage()
-    }
-    const clearEvent = ()=>{
-        titleInp.value = null 
-        dateInp.value = null 
-        locationInp.value = null 
-        attendeesInp.value = null 
-    }
+        `
+    )
+    .join("");
 
-    // deleteEvent : Deletes Events from the event list
-    const deleteEvent = ()=>{
+  weekEventsList.innerHTML = weekEvents
+    .map(
+      (event) =>
+        `<p>${event.title}, at ${event.location}, on ${event.date} (in ${event.daysAway}day${event.daysAway === 1? "":"s"})</p>`
+    )
+    .join("\n");
+};
 
-    }
+// addEvent : Adds Events from the input fields to the event list
+const addEvent = () => {
+  if (
+    !dateInp.value ||
+    !locationInp.value ||
+    !titleInp.value ||
+    !attendeesInp.value
+  ) {
+    return alert("All event field should be filled in");
+  }
+  const newEvent = {
+    date: dateInp.value,
+    location: locationInp.value,
+    title: titleInp.value,
+    attendees: attendeesInp.value,
+  };
 
-    // showWeekEvents : Shows all the events within a week
-    const showWeekEvents  = ()=>{
+  events.push(newEvent);
+  renderPage();
+};
+const clearEvent = () => {
+  titleInp.value = null;
+  dateInp.value = null;
+  locationInp.value = null;
+  attendeesInp.value = null;
+};
 
-    }
+// deleteEvent : Deletes Events from the event list
+const deleteEvent = (event) => {};
 
-    // showMostAttendees : Shows the event with the most attendees
-    const showMostAttendees = ()=>{
+// showWeekEvents : Shows all the events within a week
+const showWeekEvents = () => {
+  const daysFromNow = (date) => {
+    const today = new Date();
+    return Math.ceil(
+      (date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+    ); // (get the difference in time from today to the set date in miliseconds)/(convert it to days)
+  };
+  weekEvents = events.map((event) => {
+    const eventDate = new Date(event.date);
+    const daysAway = daysFromNow(eventDate);
+    return daysAway < 0 || daysAway > 7
+      ? null
+      : {
+          title: event.title,
+          date: event.date,
+          location: event.location,
+          attendees: event.attendees,
+          daysAway: daysAway,
+        };
+  });
+};
 
-    }
-    /* // updateEventCookies: Updates cookies of the eventsList
+// showMostAttendees : Shows the event with the most attendees
+const showMostAttendees = () => {};
+/* // updateEventCookies: Updates cookies of the eventsList
     const updateEventCookies = ()=>{
 
-        renderPage()
+        
     }
 
     // updateWeekEventCookies: Updates cookies of the eventsList
@@ -72,6 +124,6 @@ const weekEvents = []
 
     } */
 
-    addEventBtn.addEventListener("click", addEvent);
-    clearEventBtn.addEventListener("click", clearEvent);
-    renderPage()
+addEventBtn.addEventListener("click", addEvent);
+clearEventBtn.addEventListener("click", clearEvent);
+renderPage();

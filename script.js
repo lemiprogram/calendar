d5c8e9e5181482c252e2c8348330517b2339087a
wayne <waynekim07@gmail.com>
2025-05-11 14:18:25 +0300

    removed the clear btn

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const attendeesInp = document.querySelector(".attendees-inp input");
 const eventBtn = document.querySelector("#event-btn");
 const calendarBtn = document.querySelector("#calendar-btn");
 const addEventBtn = document.querySelector("#add-Event");
-const clearEventBtn = document.querySelector("#clear-Event");
 const showMostAttendeesBtn = document.querySelector(".mostAttendees-btn button")
 const closeModalBtn = document.querySelector("#close-btn")
 
@@ -86,21 +85,24 @@ const renderPage = () => {
 // addEvent : Adds Events from the input fields to the event list
 const addEvent = () => {
   if(!dateInp.value){
-    return messageModal("When is the Event? You'd need to input a Date to save an event ")
+    return messageModal("Input Error","When is the Event? You'd need to input a Date to save an event ")
   } 
   else if(!locationInp.value){
-    return messageModal("Where is it located? You'd need to input a Location to save an event ")
+    return messageModal("Input Error","Where is it located? You'd need to input a Location to save an event ")
 
   } 
   else if(!titleInp.value){
-    return messageModal("What Event is it? You'd need to input a Title to save an event")
+    return messageModal("Input Error","What Event is it? You'd need to input a Title to save an event")
   } 
   else if(!attendeesInp.value){
-    return messageModal("How many people are attending the event? You'd need to input the number of Attendees to save an event")
+    return messageModal("Input Error","How many people are attending the event? You'd need to input the number of Attendees to save an event")
   }
   else if(attendeesInp.value < 1){
-    return messageModal()
+    return messageModal("Input Error","You have to input a Positive Integer as the number of attendees")
   }
+  /* else if(String(attendeesInp.value)){
+    return messageModal("Input Error","You have to input a Positive Integer as the number of attendees")
+  } */
   const newEvent = {
     date: dateInp.value,
     location: locationInp.value,
@@ -176,7 +178,7 @@ const showMostAttendees = () => {
         }
 
     })
-    messageModal(`${eventIndex.map(i=>events[i].title).join("\n")}`)
+    messageModal( `Most Attendees (${eventCount})`, `${eventIndex.map(i=>events[i].title).join("\n")}`)
 };
     // setCookie : Creates a cookie
     function setCookie (name,value){
@@ -200,17 +202,18 @@ const showMostAttendees = () => {
         
     }
     //messageModal: prompts a message modal instead of messageModals 
-    function messageModal(msg){
-      document.querySelector(".content-text").innerText = msg
+    function messageModal(heading,msg){
       document.querySelector(".messageModal").style.display = "grid"
+      document.querySelector(".content-text").innerText = msg
+      document.querySelector(".content-heading").innerText = heading
     }
     //closeModal: closes the modal 
     function closeModal(){
-      document.querySelector(".content-text").innerText =""
       document.querySelector(".messageModal").style.display = "none"
+      document.querySelector(".content-text").innerText =""
+      document.querySelector(".content-heading").innerText = ""
     }
     addEventBtn.addEventListener("click", addEvent);
-    clearEventBtn.addEventListener("click", clearEvent);
     showMostAttendeesBtn.addEventListener("click", showMostAttendees);
     closeModalBtn.addEventListener("click",closeModal)
     window.onclick = event=>{
@@ -221,4 +224,3 @@ const showMostAttendees = () => {
 
 
 renderPage()
-console.log()
